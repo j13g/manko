40e0b6442f194cf45701f010a5307ddb3a34d725
vonas <24623262+vonas@users.noreply.github.com>
2021-03-14 12:23:42 +0100

    A pairing compares equal independent of the order of entrants

--- src/main/java/de/j13g/manko/core/Pairing.java
+++ src/main/java/de/j13g/manko/core/Pairing.java
@@ -3,6 +3,7 @@ package de.j13g.manko.core;
 import de.j13g.manko.util.UniformPair;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * A pairing between two entrants.
@@ -17,5 +18,22 @@ public class Pairing<E> extends UniformPair<E> implements Serializable {
      */
     public Pairing(E first, E second) {
         super(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pairing<?>))
+            return false;
+
+        Pairing<?> other = (Pairing<?>) o;
+        return first.equals(other.first) && second.equals(other.second)
+            || first.equals(other.second) && second.equals(other.first);
+    }
+
+    @Override
+    public int hashCode() {
+        // hash(a, b) + hash(b, a) == hash(b, a) + hash(a, b)
+        return Objects.hash(first, second) + Objects.hash(second, first);
     }
 }
