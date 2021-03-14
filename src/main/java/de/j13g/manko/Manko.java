00d3609befa3fb953f15e85229c6a46a9e2556d9
vonas <24623262+vonas@users.noreply.github.com>
2021-03-14 12:07:54 +0100

    Create a keybind for quickly opening the command prompt

--- src/main/java/de/j13g/manko/Manko.java
+++ src/main/java/de/j13g/manko/Manko.java
@@ -3,13 +3,41 @@ package de.j13g.manko;
 import de.j13g.manko.fabric.Commands;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+import java.io.File;
 
 public class Manko implements ModInitializer {
 
-	private final Commands commands = new Commands();
+	private static Commands commands;
+
+	private static KeyBinding keyOpenCommand;
 
 	@Override
 	public void onInitialize() {
+
+		commands = new Commands(MinecraftClient.getInstance());
+
+		keyOpenCommand = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.manko.tournament",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_UNKNOWN,
+				"category.manko.tournament"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (keyOpenCommand.wasPressed())
+				client.openScreen(new ChatScreen("/" + Commands.PREFIX_SHORT));
+
+			File runDir = client.runDirectory;
+		});
+
 		commands.registerCommands(ClientCommandManager.DISPATCHER);
 	}
 }
