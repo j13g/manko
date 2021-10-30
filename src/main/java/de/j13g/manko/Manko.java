fe9363c5344115520a019fbc527aa73f98684cc1
vonas <24623262+vonas@users.noreply.github.com>
2021-10-30 13:24:32 +0200

    Update to Minecraft 1.17.1

--- src/main/java/de/j13g/manko/Manko.java
+++ src/main/java/de/j13g/manko/Manko.java
@@ -7,7 +7,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
@@ -33,7 +33,7 @@ public class Manko implements ModInitializer {
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (keyOpenCommand.wasPressed())
-				client.openScreen(new ChatScreen("/" + Commands.PREFIX_SHORT));
+				client.setScreen(new ChatScreen("/" + Commands.PREFIX_SHORT));
 
 			File runDir = client.runDirectory;
 		});
